f15aed44bb858f9386b5b6ab63fb6da359a65593
analystlee <analystlee2@gmail.com>
2024-04-12 17:05:47 +0800

    add swap test finish

--- script/PoolSwapRouterTestScript.sol
+++ script/PoolSwapRouterTestScript.sol
@@ -3,21 +3,25 @@ pragma solidity ^0.8.4;
 
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
-import {PoolModifyLiquidityTest} from "v4-core/src/test/PoolModifyLiquidityTest.sol";
+import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
 import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 
-contract PoolModifyLiquidityScript is Script {
+contract PoolSwapRouterTestScript is Script {
+    using PoolIdLibrary for PoolKey;
+
     address constant POOL_MANAGER = address(0x33F048ADeCbBD8608436eF31a09db8001149404B);
     address constant TOKENA = address(0x520A3474beAaE4AC406242aa74eF6D052dE8aaED);
     address constant TOKENB = address(0x6BCCF17873Fe200962451E6824090b847DB1ACEb);
     address constant HOOK_ADDRESS = address(0x020951DEDa6928a0Eb297ed5e6a4132A01d800DD);
+    address constant POOL_SWAP = address(0x92d3117268Bd580a748acbEE73162834443a3A17);
 
-    PoolModifyLiquidityTest router = PoolModifyLiquidityTest(0x49e9cC8160410a9D8Be588f2fC9F7642e4e6E995);
+    PoolSwapTest router = PoolSwapTest(address(POOL_SWAP));
 
     function setUp() public {}
 
@@ -30,7 +34,7 @@ contract PoolModifyLiquidityScript is Script {
         /// floor(sqrt(1) * 2 ^ 96)  == 1
         bytes memory hookData = new bytes(0);
 
-        PoolKey memory pool = PoolKey({
+        PoolKey memory key = PoolKey({
             currency0: Currency.wrap(token0),
             currency1: Currency.wrap(token1),
             fee: 3000,
@@ -38,12 +42,20 @@ contract PoolModifyLiquidityScript is Script {
             hooks: IHooks(HOOK_ADDRESS)
         });
 
-        // add liquidity
+        IPoolManager.SwapParams memory params = IPoolManager.SwapParams({
+            zeroForOne: true,
+            amountSpecified: 0.1 ether,
+            sqrtPriceLimitX96: 7922816251426433759354395033
+        });
+
+        PoolSwapTest.TestSettings memory testSettings =
+                            PoolSwapTest.TestSettings({withdrawTokens: true, settleUsingTransfer: true, currencyAlreadySent: false});
+
         vm.broadcast();
         IERC20(token0).approve(address(router), type(uint256).max);
         vm.broadcast();
         IERC20(token1).approve(address(router), type(uint256).max);
         vm.broadcast();
-        router.modifyLiquidity(pool, IPoolManager.ModifyLiquidityParams(-120, 120, 100e18), hookData);
+        router.swap(key, params, testSettings, hookData);
     }
 }
